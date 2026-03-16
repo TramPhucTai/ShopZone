e3682b281181029927c8bc6383630346b334502d
PhucTai <tramphuctai2938@gmail.com>
2026-03-16 22:33:02 +0700

    Create orders using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,7 +1,8 @@
 import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOption.js";
-import { formatCurrency } from "../utils/money.js"
+import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 
 
@@ -14,7 +15,7 @@ export function renderPaymentSummary() {
     productPriceCents += product.priceCents * cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
-    
+
     shippingPriceCents += deliveryOption.priceCents;
   });
 
@@ -26,8 +27,8 @@ export function renderPaymentSummary() {
 
   const totalItems = calculateCartQuantity();
 
-  const paymentSummaryHTML = 
-  `
+  const paymentSummaryHTML =
+    `
   <div class="payment-summary-title">
     Order Summary
   </div>
@@ -57,11 +58,35 @@ export function renderPaymentSummary() {
     <div class="payment-summary-money js-payment-summary-total">$${formatCurrency(totalCents)}</div>
   </div>
 
-  <button class="place-order-button button-primary">
+  <button class="place-order-button button-primary js-place-order">
     Place your order
   </button>
   `;
 
   document.querySelector('.js-payment-summary').innerHTML
     = paymentSummaryHTML
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+
+        const order = await response.json();
+        addOrder(order);
+
+      } catch (error) {
+        console.log('Unexpected error. Please try again later.')
+      }
+
+      window.location.href = 'orders.html';
+    })
 }
